ed18586ae43e4a4ccdc6c27b9a02d044bae04e32
Fajar Rizky <fajarturbouye@gmail.comcd>
2017-10-24 11:06:29 +0700

    Initial commit

---
+++ app/src/main/java/com/fajarzky/jsmobileapps/detail_kategori.java
@@ -0,0 +1,13 @@
+package com.fajarzky.jsmobileapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class detail_kategori extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_kategori);
+    }
+}
